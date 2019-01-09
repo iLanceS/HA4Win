616dbbf69bb7b1be84bff653409c02b6e6c1e28c
Lance <Lance@LANCE_GRAM>
2019-01-09 17:59:29 +0800

    second commit;

---
+++ Controls/DoubleTextBox.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ha4win.Controls
+{
+    /// <summary>
+    /// DoubleTextBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class DoubleTextBox : UserControl
+    {
+        public DoubleTextBox()
+        {
+            InitializeComponent();
+        }
+
+
+        public String V1
+        {
+            get { return (String)GetValue(V1Property); }
+            set { SetValue(V1Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for V1.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty V1Property = DependencyProperty.Register("V1", typeof(String), typeof(DoubleTextBox), new PropertyMetadata(""));
+
+
+
+
+
+        public String V2
+        {
+            get { return (String)GetValue(v2Property); }
+            set { SetValue(v2Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for v2.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty v2Property = DependencyProperty.Register("V2", typeof(String), typeof(DoubleTextBox), new PropertyMetadata(""));
+    }
+}
---
+++ Model/Entities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ha4win.Model
+{
+    public class Info
+    {
+        /// <summary>
+        /// 业务虚拟IP网络接口
+        /// </summary>
+        public String VSInterface = "";
+        /// <summary>
+        /// 业务启动脚本
+        /// </summary>
+        public String VSStartScript = "";
+
+        /// <summary>
+        /// 运行模式
+        /// </summary>
+        public RunType RunType = RunType.主控端;
+
+        /// <summary>
+        /// 心跳信息-->心跳丢失后启用对外服务网络接口 并设置IP
+        /// </summary>
+        public HeartInfo HeartInfo = new HeartInfo();
+        /// <summary>
+        /// 业务虚拟IP
+        /// </summary>
+        public IPInfo VSIPInfo = new IPInfo();
+    }
+
+
+
+    public class IPInfo
+    {
+        public string ip;
+        public string mask;
+        public string getway;
+    }
+
+    public class HeartInfo
+    {
+        public string romoteIP;
+        public int localPort = 5678;
+        public int romotePort = 5678;
+
+        public DateTime LastReciveTime = new DateTime();
+    }
+
+
+    public enum RunType
+    {
+        主控端,
+        从控端
+    }
+
+
+}
